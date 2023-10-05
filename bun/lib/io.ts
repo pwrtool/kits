85a8f311a0e42f632705335d86a7d5016dabfc24
firesquid <jdeiss06@gmail.com>
2023-10-05 17:32:22 -0500

    add dichotomous question

--- bun/lib/io.ts
+++ bun/lib/io.ts
@@ -1,4 +1,38 @@
-class IO {
+import readline from "readline";
+
+export class IO {
+  static async prompt(question: string): Promise<string> {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const answer = await new Promise<string>((resolve) => {
+      rl.question(`\x1b[37;1m${question}\x1b[0m`, (answer) => {
+        resolve(answer);
+      });
+    });
+
+    rl.close();
+    return answer;
+  }
+
+  static async dichotomous(question: string): Promise<boolean> {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const answer = await new Promise<boolean>((resolve) => {
+      rl.question(`\x1b[37;1m${question}\x1b[0m`, (answer) => {
+        resolve(answer.toLowerCase() === "y");
+      });
+    });
+
+    rl.close();
+    return answer;
+  }
+
   // This article was very helpful:
   // https://notes.burke.libbey.me/ansi-escape-codes/
 
@@ -7,7 +41,7 @@ class IO {
    * @param message - The message to output
    * @return void
    */
-  out(message: string) {
+  static out(message: string) {
     console.log(message);
   }
 
@@ -16,7 +50,7 @@ class IO {
    * @param message - The message to output
    * @return void
    */
-  success(message: string) {
+  static success(message: string) {
     console.log("\x1b[32;1m%s\x1b[0m", message);
   }
 
@@ -25,7 +59,7 @@ class IO {
    * @param message - The message to output
    * @return void
    */
-  bold(message: string) {
+  static bold(message: string) {
     console.log("\x1b[37;1m%s\x1b[0m", message);
   }
 
@@ -34,7 +68,7 @@ class IO {
    * @param message - The message to output
    * @return void
    */
-  warn(message: string) {
+  static warn(message: string) {
     console.log("\x1b[33;1m%s\x1b[0m", message);
   }
 
@@ -43,7 +77,7 @@ class IO {
    * @param message - The message to output
    * @return void
    */
-  header(message: string) {
+  static header(message: string) {
     console.log("\x1b[34;1;4m%s\x1b[0m", message);
   }
 
@@ -52,7 +86,7 @@ class IO {
    * @param message - The message to output
    * @return void
    */
-  error(message: string) {
+  static error(message: string) {
     console.log("\x1b[31;1m%s\x1b[0m", message);
   }
 }
