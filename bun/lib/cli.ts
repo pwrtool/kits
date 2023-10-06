36d940b1aee53ea6285e49cf5f83599520fc1225
firesquid <jdeiss06@gmail.com>
2023-10-05 22:22:50 -0500

    add better jsdocs

--- bun/lib/cli.ts
+++ bun/lib/cli.ts
@@ -1,5 +1,7 @@
 import { ParsedRunstring } from "@pwrtool/runstring";
-
+/**
+ * Wraps the map containing the arguments provided to the tool in the CLI
+ */
 export class CLIArgs {
   private args: Map<string, string> = new Map();
 
@@ -15,6 +17,11 @@ export class CLIArgs {
     return this.args.has(key);
   }
 
+  /**
+   * Gets a value. If that value is not found, it throws an error.
+   * @param The name of the argument to get
+   * @return The value of the argument
+   */
   getOrThrow(key: string): string {
     const value = this.get(key);
     if (value === undefined) {
