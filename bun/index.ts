5233509275cd86d648942b4513d555a1a13b1166
firesquid6 <jdeiss06@gmail.com>
2023-10-07 10:35:50 -0500

    add exiting with success and exiting with failure

--- bun/index.ts
+++ bun/index.ts
@@ -6,4 +6,14 @@
 
 import { powertool, findRunstring, Kit } from "./lib/kit";
 
-export default { powertool, findRunstring, Kit };
+export default powertool;
+
+export { findRunstring, Kit };
+
+export function exitWithSuccess() {
+  process.exit(0);
+}
+
+export function exitWithError() {
+  process.exit(1);
+}
