8a566fb624512615a5f6fb27ae9dedaee697478e
firesquid <jdeiss06@gmail.com>
2023-11-03 20:50:01 -0500

    remove borked tests

--- bun/tests/io.manual.ts
+++
@@ -1,8 +0,0 @@
-//idk how to test this other than just manually verifying it
-import { IO } from "../lib/io";
-
-const answer = await IO.prompt("Enter a message: ");
-IO.out(answer);
-
-const sure = await IO.dichotomous("Are you sure? ");
-IO.out(sure ? "Yes" : "No");
