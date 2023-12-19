12e30e97c3f4fa3d1259a469f43cd7af9858a0fb
firesquid6 <jdeiss06@gmail.com>
2023-12-19 11:11:36 -0600

    add tool runner

--- bun/index.ts
+++ bun/index.ts
@@ -1,4 +1,8 @@
 // see core/lib.go in the powertool repository
+
+import { Tool, makeToolRunner } from "./lib/runner";
+import { parseArgv } from "./lib/parser";
+
 // this is passed to the kit instructing it on what to run
 export interface Rundata {
   tool: string;
@@ -7,4 +11,19 @@ export interface Rundata {
   mockInputs: string[];
   runDirectory: string;
   silent: boolean;
+}
+
+export default function powertool(tools: Tool[]) {
+  const runner = makeToolRunner();
+  tools.forEach((tool) => {
+    runner.addTool(tool);
+  });
+
+  const rundata = parseArgv(process.argv);
+
+  try {
+    runner.run(rundata);
+  } catch (e) {
+    console.log(`\x1b[31mERROR: ${e}`);
+  }
 }
